c41cc5908e2f434880eadd70a5244736cfe813f6
Jose Luiz Coe <jose.souza@catho.com>
2019-06-01 02:26:32 -0300

    Criando componente Signup

---
+++ starter-code/src/Signup.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import NavBar from './NavBar';
+import FormField from './FormField';
+import CoolButton from './CoolButton';
+
+
+class Signup extends Component {
+
+    render() {
+        return (
+            <div>
+                <NavBar />
+                <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+                <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+                <CoolButton isSmall isDanger className="is-rounded my-class">Button 1</CoolButton>
+                <CoolButton isSmall isSuccess>Button 2</CoolButton>
+            </div>
+        );
+    }
+}
+
+export default Signup;
